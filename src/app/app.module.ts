91a828eaf54e8f095f7ed9534b7a2b255808536e
jeeeanlefchak <jean_2103@hotmail.com>
2019-05-30 13:01:22 -0300

    firebase addtiong

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,17 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { AngularFireModule } from 'angularfire2';
+import { environment } from 'src/environments/environment';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase)
+  ],
   providers: [
     StatusBar,
     SplashScreen,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAG9CvIX9SDfF0P6DQ77vHdPCJN9sH_vUc",
+    authDomain: "restaura-res.firebaseapp.com",
+    databaseURL: "https://restaura-res.firebaseio.com",
+    projectId: "restaura-res",
+    storageBucket: "restaura-res.appspot.com",
+    messagingSenderId: "127229625839",
+    appId: "1:127229625839:web:61c37d08bcfb427b"
+  }
 };
 
 /*
