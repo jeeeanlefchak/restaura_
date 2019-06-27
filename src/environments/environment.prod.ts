5d3297a0c8e320759d97d8cad2f0551c34feeb7f
jeeeanlefchak <jean_2103@hotmail.com>
2019-06-27 13:06:52 -0300

    build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAG9CvIX9SDfF0P6DQ77vHdPCJN9sH_vUc",
+    authDomain: "restaura-res.firebaseapp.com",
+    databaseURL: "https://restaura-res.firebaseio.com",
+    projectId: "restaura-res",
+    storageBucket: "restaura-res.appspot.com",
+    messagingSenderId: "127229625839",
+    appId: "1:127229625839:web:61c37d08bcfb427b"
+  }
 };
